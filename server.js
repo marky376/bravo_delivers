c1d841008b85159d3ead415e53e43e739f29cb32
marky376 <mmutuamark@gmail.com>
2024-09-12 11:50:35 +0300

    Starting my server and importing necessities

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './lib/middlewares';
+
+
+const server = express();
+
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
+
+export default server;
