c61b53ab6c0601340364427f937fd885adabc9b3
marky376 <mmutuamark@gmail.com>
2024-09-12 23:09:18 +0300

    Setting up the database

---
+++ config/databse.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT,
+    }
+);
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = { sequelize, connectDB };
