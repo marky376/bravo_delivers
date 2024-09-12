452810fd4a7358686ad6667cbbee79072e0327e0
marky376 <mmutuamark@gmail.com>
2024-09-13 00:06:50 +0300

    Establishing connection with my Database

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './lib/middlewares';
+import { connectDB } from './database'; // Import the connection
 
 
 const server = express();
 
 injectMiddlewares(server);
 injectRoutes(server);
+connectDB(); // Connect to the database
 startServer(server);
 
 export default server;
