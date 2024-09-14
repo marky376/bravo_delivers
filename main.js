ac8009b9048b30a72d49c4c1306c55f743d7d399
marky376 <mmutuamark@gmail.com>
2024-09-14 11:45:09 +0300

    Working on the cart functionality

--- main.js
+++ main.js
@@ -12,3 +12,34 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
 });
+
+// Cart Functionality
+let cart = [];
+
+// Open/Close Cart
+const cartIcon = document.querySelector('.fa-shopping-cart');
+const cartContainer = document.createElement('div');
+cartContainer.classList.add('cart-container');
+document.body.appendChild(cartContainer);
+
+// Cart Template
+function updateCartUI() {
+  cartContainer.innerHTML = `
+    <div class="cart-header">
+      <h3>Your Cart</h3>
+      <button class="close-cart">X</button>
+    </div>
+    <ul class="cart-items">
+      ${cart.map((item, index) => `
+        <li class="cart-item">
+          <span>${item.name}</span>
+          <span>$${item.price}</span>
+          <button class="remove-item" data-index="${index}">Remove</button>
+        </li>
+          `).join('')}
+    </ul>
+    <div class="cart-total">
+        <h4>Total: $${cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}</h4>
+    </div>
+  `;
+}
