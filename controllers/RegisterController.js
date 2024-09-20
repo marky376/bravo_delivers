b8b97cb1e2ac0aa27bf262a9d6bc6610aa1d7ff3
marky376 <mmutuamark@gmail.com>
2024-09-20 05:19:34 +0300

    Adding some Registration  service functionalities

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,10 +1,15 @@
 import UserService from '../services/UserService.js';
 
-const registerUser = async (req, res, next) => {
-    try {
-        const newUser = await UserService.register(req.body);
-        res.status(201).json(newUser);
-    } catch (error) {
-        next(error);
+const RegisterController = {
+    async registerUser(req, res, next) {
+        try {
+            const newUser = await UserService.register(req.body);
+            res.status(201).json(newUser);
+        } catch (error) {
+            next(error);
+        }
     }
 };
+
+// Export the RegisterController object itself
+export default RegisterController;
