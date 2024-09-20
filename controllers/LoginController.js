8b2c90da3f47c8298af50ed522851026c31d5990
marky376 <mmutuamark@gmail.com>
2024-09-20 05:17:24 +0300

    Adding some Login functionalities

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,10 +1,18 @@
 import UserService from '../services/UserService.js';
 
-const loginUser = async (req, res, next) => {
-    try {
-        const token = await UserService.login(req.body.email, req.body.password);
-        res.status(200).json({ token });
-    } catch (error) {
-        next(error);
+const LoginController = {
+    /**
+     * Log in a user.
+     */
+    async loginUser(req, res, next) {
+        try {
+            const token = await UserService.login(req.body.email, req.body.password);
+            res.status(200).json({ token });
+        } catch (error) {
+            next(error);
+        }
     }
 };
+
+// Export the LoginController object
+export default LoginController;
